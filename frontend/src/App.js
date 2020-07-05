e70e12a0698f2bc3216418dc904595777dfad43a
Paul Soriano <pvsoriano@mifune.local>
2020-07-05 15:14:17 -0400

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NavBar from "./NavBar";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <p>TEST</p>
+            </div>
+        );
+    }
+}
+
+export default App;

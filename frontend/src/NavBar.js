f41a23d535cdaff6bc8dafba67ef1096a00e42d9
Paul Soriano <pvsoriano@mifune.local>
2020-07-06 22:35:58 -0400

    updated navbar.js

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function NavBar() {
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="#">RPS Consulting Group</a>
+            <a class="navbar-brand" href="#Main">RPS Consulting Group</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -12,13 +12,13 @@ function NavBar() {
             <div class="collapse navbar-collapse" id="navbarColor01">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#Home">Home<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a class="nav-link" href="#About">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contact Us</a>
+                        <a class="nav-link" href="#ContactUs">Contact Us</a>
                     </li>
                 </ul>
             </div>
